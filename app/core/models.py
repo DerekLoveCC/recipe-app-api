6aa427db065d7249de50a8d7dc8b66a4efc0bb5c
Derek <derek@smallboat.com>
2022-09-04 11:21:18 +0800

    super user

--- app/core/models.py
+++ app/core/models.py
@@ -15,6 +15,14 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """user in the system"""
@@ -26,5 +34,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
-
